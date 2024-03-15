38722f4f1dab48388decd26de0d9a89a06cab1f3
Dina Berry <dinaberry@outlook.com>
2024-03-15 13:05:22 +0000

    get single list works

--- scripts/curl.sh
+++ scripts/curl.sh
@@ -1,7 +1,13 @@
 
-LIST
+## LIST
 
+# Get All lists
 curl -X "GET" -H "Content-Type: application/json"  http://localhost:8000/lists --verbose
+
+# Get list by id
+curl -X "GET" -H "Content-Type: application/json"  http://localhost:8000/lists/65f4421a7157d5922f45d5d3 --verbose
+
+# Create list
 curl -X POST \
 -H "Content-Type: application/json" \
 -d @insert_list.json \
